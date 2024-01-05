26b8b0dd1243ee8173605c18f6caf447857b99be
Favour Olusayo <fsticks8187@gmail.com>
2024-01-05 23:25:29 +0100

    Refactor DiscordAuthentication class in oauth2/auth.py

--- oauth2/auth.py
+++ oauth2/auth.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.backends import BaseBackend
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from oauth2.utils import (
@@ -13,7 +12,26 @@ from talents.models import Talent
 
 
 class DiscordAuthentication(BaseAuthentication):
+    """
+    Handles authentication using Discord OAuth2.
+
+    This class provides methods for authenticating users, retrieving access tokens,
+    refreshing tokens, and revoking tokens.
+    """
+
     def authenticate(self, request) -> Talent:
+        """
+        Authenticates the request using the provided token and returns the corresponding Talent object.
+
+        Args:
+            request: The request object containing the token in the Authorization header.
+
+        Returns:
+            A tuple containing the authenticated Talent object and any potential error message.
+
+        Raises:
+            AuthenticationFailed: If the Authorization header is missing or if there is an error getting the user.
+        """
         token = request.headers.get("Authorization")
 
         if token is None:
@@ -33,7 +51,21 @@ class DiscordAuthentication(BaseAuthentication):
                 talent = Talent.objects.create_talent(user)
             return (talent, None)
 
-    def get_access_token(self, code):
+    def get_access_token(self, code) -> tuple:
+        """
+        Retrieves the access token, refresh token, and expiration time for the given authorization code.
+
+        Args:
+            code (str): The authorization code.
+
+        Returns:
+            tuple: A tuple containing the access token, refresh token, and expiration time.
+                    The access token is a string in the format "Bearer <access_token>".
+                    The refresh token is a string in the format "Bearer <refresh_token>".
+                    The expiration time is an integer representing the number of seconds until the access token expires.
+        Raises:
+            AuthenticationFailed: If an error occurs while retrieving the access token.
+        """
         try:
             credentials = exchange_code(code)
             access_token = f"Bearer {get_access_token(credentials)}"
@@ -43,7 +75,19 @@ class DiscordAuthentication(BaseAuthentication):
         except Exception as e:
             raise AuthenticationFailed(f"{e}")
 
-    def refresh_token(self, refresh_token):
+    def refresh_token(self, refresh_token) -> tuple:
+        """
+        Refreshes the access token using the provided refresh token.
+
+        Args:
+            refresh_token (str): The refresh token to use for refreshing the access token.
+
+        Returns:
+            tuple: A tuple containing the new access token and its expiration time in seconds.
+
+        Raises:
+            AuthenticationFailed: If an error occurs while refreshing the access token.
+        """
         try:
             credentials = refresh_credentials(refresh_token)
             new_access_token = f"Bearer {get_access_token(credentials)}"
@@ -52,7 +96,19 @@ class DiscordAuthentication(BaseAuthentication):
         except Exception as e:
             raise AuthenticationFailed(f"{e}")
 
-    def revoke_token(self, access_token):
+    def revoke_token(self, access_token) -> bool:
+        """
+        Revoke the specified access token.
+
+        Args:
+            access_token (str): The access token to be revoked.
+
+        Returns:
+            bool: True if the access token was successfully revoked, False otherwise.
+
+        Raises:
+            AuthenticationFailed: If an error occurs while revoking the access token.
+        """
         try:
             is_revoked = revoke_access_token(access_token)
             return is_revoked
